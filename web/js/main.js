8ef847f2e571d261b072dfb19a99ee4a46f5c0e6
Adam Fister <arfister@gmail.com>
2019-03-11 22:25:11 -0400

    add zeroes to prices which require them, formatting

--- web/js/main.js
+++ web/js/main.js
@@ -13,9 +13,14 @@ function getAppetizers() {
                 let app = JSON.stringify(myJson.menu_items[i].description);
                 app = app.slice(1, -1);
 
-                document.getElementById("apps" + i).innerHTML = app + " $" + Math.round(Math.random() * 10000) / 100;
-
+                app += app + " $" + Math.round(Math.random() * 10000) / 100;
+                console.log(app);
+                if (app.charAt(app.length - 2) == ".") {
+                    app += "0";
+                }
+                document.getElementById("apps" + i).innerHTML = app;
             }
+
         }
         ).catch(function (error) {
             console.log(error);
@@ -35,9 +40,14 @@ function getMains() {
                 let main = JSON.stringify(myJson.menu_items[i].description);
                 main = main.slice(1, -1);
 
-                document.getElementById("main" + i).innerHTML = main + " $" + Math.round(Math.random() * 10000) / 100;
-
+                main += main + " $" + Math.round(Math.random() * 10000) / 100;
+                console.log(main);
+                if (main.charAt(main.length - 2) == ".") {
+                    main += "0";
+                }
+                document.getElementById("main" + i).innerHTML = main;
             }
+
         }
         ).catch(function (error) {
             console.log(error);
@@ -57,9 +67,15 @@ function getSides() {
                 let sides = JSON.stringify(myJson.menu_items[i].description);
                 sides = sides.slice(1, -1);
 
-                document.getElementById("sides" + i).innerHTML = sides + " $" + Math.round(Math.random() * 10000) / 100;
+                sides += sides + " $" + Math.round(Math.random() * 10000) / 100;
+                console.log(sides);
+                if (sides.charAt(sides.length - 2) == ".") {
+                    sides += "0";
+                }
+                document.getElementById("sides" + i).innerHTML = sides;
 
             }
+
         }
         ).catch(function (error) {
             console.log(error);
@@ -79,9 +95,15 @@ function getBreakfasts() {
                 let breakfast = JSON.stringify(myJson.menu_items[i].description);
                 breakfast = breakfast.slice(1, -1);
 
-                document.getElementById("breakfast" + i).innerHTML = breakfast + " $" + Math.round(Math.random() * 10000) / 100;
+                breakfast += breakfast + " $" + Math.round(Math.random() * 10000) / 100;
+                console.log(breakfast);
+                if (breakfast.charAt(breakfast.length - 2) == ".") {
+                    breakfast += "0";
+                }
+                document.getElementById("breakfast" + i).innerHTML = breakfast;
 
             }
+
         }
         ).catch(function (error) {
             console.log(error);
