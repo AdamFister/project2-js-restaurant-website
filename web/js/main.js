a097daabb8b12656b52d962b859ee296e3b4ac7f
Adam Fister <arfister@gmail.com>
2019-03-11 21:50:02 -0400

    format

--- web/js/main.js
+++ web/js/main.js
@@ -7,14 +7,14 @@ function getAppetizers() {
             return response.json();
         })
         .then(function (myJson) {
-            
+
             for (let i = 0; i < 8; i++) {
 
-            let app = JSON.stringify(myJson.menu_items[i].description);
-            app = app.slice(1, -1);
+                let app = JSON.stringify(myJson.menu_items[i].description);
+                app = app.slice(1, -1);
 
-            document.getElementById("apps" + i).innerHTML = app + " $" + Math.round(Math.random() * 10000) / 100;
-            
+                document.getElementById("apps" + i).innerHTML = app + " $" + Math.round(Math.random() * 10000) / 100;
+
             }
         }
         ).catch(function (error) {
@@ -25,68 +25,68 @@ function getAppetizers() {
 
 function getMains() {
     fetch('https://entree-f18.herokuapp.com/v1/menu/8')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (myJson) {
-        
-        for (let i = 0; i < 8; i++) {
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (myJson) {
 
-        let main = JSON.stringify(myJson.menu_items[i].description);
-        main = main.slice(1, -1);
+            for (let i = 0; i < 8; i++) {
 
-        document.getElementById("main" + i).innerHTML = main + " $" + Math.round(Math.random() * 10000) / 100;;
-        
+                let main = JSON.stringify(myJson.menu_items[i].description);
+                main = main.slice(1, -1);
+
+                document.getElementById("main" + i).innerHTML = main + " $" + Math.round(Math.random() * 10000) / 100;
+
+            }
         }
-    }
-    ).catch(function (error) {
-        console.log(error);
+        ).catch(function (error) {
+            console.log(error);
 
-    });
+        });
 }
 
 function getSides() {
     fetch('https://entree-f18.herokuapp.com/v1/menu/8')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (myJson) {
-        
-        for (let i = 0; i < 8; i++) {
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (myJson) {
 
-        let sides = JSON.stringify(myJson.menu_items[i].description);
-        sides = sides.slice(1, -1);
+            for (let i = 0; i < 8; i++) {
 
-        document.getElementById("sides" + i).innerHTML = sides + " $" + Math.round(Math.random() * 10000) / 100;;
-        
+                let sides = JSON.stringify(myJson.menu_items[i].description);
+                sides = sides.slice(1, -1);
+
+                document.getElementById("sides" + i).innerHTML = sides + " $" + Math.round(Math.random() * 10000) / 100;
+
+            }
         }
-    }
-    ).catch(function (error) {
-        console.log(error);
+        ).catch(function (error) {
+            console.log(error);
 
-    });
+        });
 }
 
 function getBreakfasts() {
     fetch('https://entree-f18.herokuapp.com/v1/menu/8')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (myJson) {
-        
-        for (let i = 0; i < 8; i++) {
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (myJson) {
 
-        let breakfast = JSON.stringify(myJson.menu_items[i].description);
-        breakfast = breakfast.slice(1, -1);
+            for (let i = 0; i < 8; i++) {
 
-        document.getElementById("breakfast" + i).innerHTML = breakfast + " $" + Math.round(Math.random() * 10000) / 100;;
-        
+                let breakfast = JSON.stringify(myJson.menu_items[i].description);
+                breakfast = breakfast.slice(1, -1);
+
+                document.getElementById("breakfast" + i).innerHTML = breakfast + " $" + Math.round(Math.random() * 10000) / 100;
+
+            }
         }
-    }
-    ).catch(function (error) {
-        console.log(error);
+        ).catch(function (error) {
+            console.log(error);
 
-    });
+        });
 }
 
 
